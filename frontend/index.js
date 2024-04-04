a53d73b1fec4bc5cd1490dc164baaeff16a8ce40
Rushabh Jaiswal <>
2024-04-04 00:05:57 -0700

    adding features and bugfixes

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,7 @@ let directionsRenderers = [];
 // API related constants
 const API_BASE_URL = 'http://localhost:8080';
 const COORD_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json";
-const API_KEY = "YOUR_API_KEY";
+var API_KEY = null;
 
 // Color Map
 const colorDict = {
@@ -16,6 +16,7 @@ const colorDict = {
     "30-70": "#3A548D"
 };
 
+// Legend Mappings
 const colorDictLegend = {
     "shaded": "Most Shaded Path",
     "shortest": "Shortest Path",
@@ -23,12 +24,41 @@ const colorDictLegend = {
     "70-30": "70% Distance - 30% Shaded Path Ratio",
     "30-70": "30% Distance - 70% Shaded Path Ratio"
 };
-const colorHues = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#8B00FF', '#FF00FF'];
-const green = '#28A745';
 
+// Map Realted Variables
+var mask = null;
+var spinner = null;
+var polylineData = [];
+var markerData = [];
+const startMarkerIcon = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+const stopMarkerIcon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+
+// Load the Google Maps JavaScript API
 function loadGoogleMaps() {
+    // Load the Google Maps JavaScript API    
     (g => {
-        var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
+        var h, a, k, p = "The Google Maps JavaScript API",
+            c = "google",
+            l = "importLibrary",
+            q = "__ib__",
+            m = document,
+            b = window;
+        b = b[c] || (b[c] = {});
+        var d = b.maps || (b.maps = {}),
+            r = new Set,
+            e = new URLSearchParams,
+            u = () => h || (h = new Promise(async (f, n) => {
+                await (a = m.createElement("script"));
+                e.set("libraries", [...r] + "");
+                for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+                e.set("callback", c + ".maps." + q);
+                a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                d[q] = f;
+                a.onerror = () => h = n(Error(p + " could not load."));
+                a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                m.head.append(a)
+            }));
+        d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
     })({
         key: API_KEY,
         v: "weekly",
@@ -37,42 +67,88 @@ function loadGoogleMaps() {
     });
 }
 
-function refreshShadedPaths(source, destination, numRoutes, mode) {
-
+// Refresh the shaded paths on the map
+function refreshShadedPaths(source, destination, travelMode) {
     removeLegend();
+
+    // Show the spinner
+    mask.style.display = 'block';
+    spinner.style.display = 'block';
+
+    removeExistingPaths(polylineData);
     Promise.all([
         getCoordinates(source),
         getCoordinates(destination)
     ]).then(([coords1, coords2]) => {
-        getPaths(coords1, coords2, numRoutes, mode)
+        getPaths(coords1, coords2, travelMode)
             .then(pathsData => {
+                // Hide the spinner
+                mask.style.display = 'none';
+                spinner.style.display = 'none';
                 // Render the paths on the map
                 renderPaths(pathsData);
+                createLegend();
             })
-            .catch(error => console.error('Error:', error));
-        createLegend();
-    }).catch(error => console.error('Error:', error));
+            .catch(error => {
+                console.error('Error:', error);
+
+                // Hide the spinner
+                spinner.style.display = 'none';
+            });
+
+    }).catch(error => {
+        console.error('Error:', error);
+        alert('Please enter a more specific location, no valid path found.');
+
+        // Hide the spinner
+        spinner.style.display = 'none';
+        return;
+    });
 
 }
 
 // DOMContentLoaded event listener
 document.addEventListener('DOMContentLoaded', () => {
-    loadGoogleMaps();
+    mask = document.getElementById('mask');
+    spinner = document.getElementById('loadingSpinner');
+    document.getElementById('apiKey').addEventListener('input', function() {
+        API_KEY = this.value;
+        validateKey();
+    });
 
     const searchButton = document.getElementById('searchButton');
     searchButton.addEventListener('click', () => {
-        const sourceValue = document.getElementById('sourceInput').value;
-        const destinationValue = document.getElementById('destinationInput').value;
-        const numRoutes = parseInt(document.getElementById('numRoutesInput').value);
-        const travelMode = document.querySelector('input[name="travelMode"]:checked').value;
+        let sourceValue = document.getElementById('sourceInput').value;
+        let destinationValue = document.getElementById('destinationInput').value;
+        let travelMode = document.querySelector('input[name="travelMode"]:checked').value;
 
         // Refresh the map and fetch new routes based on input values
-        refreshShadedPaths(sourceValue, destinationValue, numRoutes, travelMode);
+        refreshShadedPaths(sourceValue, destinationValue, travelMode);
     });
 });
 
-async function initMap(position = { lng: -111.9412691, lat: 33.4243385 }) { // Default is Paris -111.9412691,33.4243385
-    const { Map } = await google.maps.importLibrary("maps");
+function validateKey() {
+    fetch(`${COORD_BASE_URL}?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'REQUEST_DENIED') {
+                console.error('Invalid API key');
+            } else {
+                loadGoogleMaps();
+                initMap();
+                mask.style.display = 'none';
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+async function initMap(position = {
+    lng: -111.9412691,
+    lat: 33.4243385
+}) { // Default is Paris -111.9412691,33.4243385
+    const {
+        Map
+    } = await google.maps.importLibrary("maps");
 
     // The map, centered at the provided position or default
     map = new Map(document.getElementById("map"), {
@@ -82,12 +158,79 @@ async function initMap(position = { lng: -111.9412691, lat: 33.4243385 }) { // D
     });
 }
 
+// Mark the start and stop points on the map
+function markStartAndStop(map, start, stop) {
+    markerData = [];
+    let startIcon = new google.maps.Marker({
+        position: start,
+        map: map,
+        title: 'Start',
+        icon: {
+            url: startMarkerIcon,
+            scaledSize: new google.maps.Size(45, 45)
+        }
+    });
+
+    // Create the stop marker
+    let stopIcon = new google.maps.Marker({
+        position: stop,
+        map: map,
+        title: 'Stop',
+        icon: {
+            url: stopMarkerIcon,
+            scaledSize: new google.maps.Size(45, 45)
+        }
+    });
+    markerData.push(startIcon);
+    markerData.push(stopIcon);
+}
+
+// Function to remove all polyline markers and paths
+function removeExistingPaths() {
+
+    markerData.forEach(marker => {
+        // Remove the marker
+        if (marker && typeof marker.setMap === 'function') {
+            marker.setMap(null);
+        }
+    });
+
+    // Remove the polyline
+    polylineData.forEach(polyline => {
+        // Remove the polyline
+        if (polyline && typeof polyline.setMap === 'function') {
+            polyline.setMap(null);
+        }
+    });
+}
+
+// Render the paths on the map
 function renderPaths(pathsData) {
     let bounds = new google.maps.LatLngBounds();
     let infoWindow = new google.maps.InfoWindow();
 
+
+    if (!pathsData || pathsData.length === 0) {
+        alert('Wrong input location or no valid path found.');
+        return;
+    }
+
+    let op = pathsData[0];
+    markStartAndStop(map, {
+        lat: op.path[0][1],
+        lng: op.path[0][0]
+    }, {
+        lat: op.path[op.path.length - 1][1],
+        lng: op.path[op.path.length - 1][0]
+    });
+    polylineData = [];
+
+
     pathsData.forEach(pathObj => {
-        const formattedPath = pathObj.path.map(coord => ({ lat: coord[1], lng: coord[0] }));
+        const formattedPath = pathObj.path.map(coord => ({
+            lat: coord[1],
+            lng: coord[0]
+        }));
         const path = new google.maps.Polyline({
             path: formattedPath,
             geodesic: true,
@@ -95,13 +238,20 @@ function renderPaths(pathsData) {
             strokeOpacity: 1,
             strokeWeight: 8
         });
+        polylineData.push(path);
 
         // Add a listener for the mouseover event
-        path.addListener('mouseover', function (event) {
-            path.setOptions({ strokeWeight: 10 });  // Increase stroke weight
+        path.addListener('mouseover', function(event) {
+            path.setOptions({
+                strokeWeight: 12
+            }); // Increase stroke weight
 
             // Set the content and position of the InfoWindow
-            infoWindow.setContent(`The selected path is ${pathObj.typeOfPath} path.`);
+            infoWindow.setContent(`
+            <div style="font-family: Arial, sans-serif; margin: 10px;">
+                <p style="margin-bottom: 0; font-size: 1rem;">The selected path is <strong>${colorDictLegend[pathObj.typeOfPath]}</strong>.</p>
+            </div>
+            `);
             infoWindow.setPosition(event.latLng);
 
             // Open the InfoWindow
@@ -109,8 +259,10 @@ function renderPaths(pathsData) {
         });
 
         // Add a listener for the mouseout event
-        path.addListener('mouseout', function () {
-            path.setOptions({ strokeWeight: 8 });  // Reset stroke weight
+        path.addListener('mouseout', function() {
+            path.setOptions({
+                strokeWeight: 8
+            }); // Reset stroke weight
 
             // Close the InfoWindow
             infoWindow.close();
@@ -129,12 +281,8 @@ function renderPaths(pathsData) {
     map.fitBounds(bounds);
 }
 
-window.onload = function () {
-    loadGoogleMaps();
-    initMap();
-};
-
-async function getPaths(origin, destination, numRoutes, mode) {
+// Fetch the paths from the backend API
+async function getPaths(origin, destination, travelMode) {
     const response = await fetch(`${API_BASE_URL}/get-path`, {
         method: 'POST',
         headers: {
@@ -143,8 +291,7 @@ async function getPaths(origin, destination, numRoutes, mode) {
         body: JSON.stringify({
             origin: origin,
             destination: destination,
-            travelMode: google.maps.TravelMode[mode],
-            numRoutes: numRoutes
+            travelMode: travelMode
         }),
     });
 
@@ -156,18 +303,23 @@ async function getPaths(origin, destination, numRoutes, mode) {
     return data;
 }
 
+// Get coordinates of an address from the Geocoding API
 async function getCoordinates(address) {
     let url = `${COORD_BASE_URL}?address=${encodeURIComponent(address)}&key=${API_KEY}`
     const response = await fetch(url);
     const data = await response.json();
     if (data.status === 'OK') {
-        const { lat, lng } = data.results[0].geometry.location;
+        const {
+            lat,
+            lng
+        } = data.results[0].geometry.location;
         return [lat, lng];
     } else {
         throw new Error('Geocoding API request failed');
     }
 }
 
+// Remove the legend from the map
 function removeLegend() {
     // Check if the legend already exists
     let oldLegend = document.getElementById('legend');
@@ -177,17 +329,18 @@ function removeLegend() {
     }
 }
 
+// Create the legend and add it to the map
 function createLegend() {
 
     // Create main div
     let div = document.createElement('div');
     div.id = 'legend';
-    div.style.cssText = 'background-color: #D3D3D3; padding: 10px; text-align: left; font-family: Arial, sans-serif; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);';
+    div.style.cssText = 'background-color: #D3D3D3; padding: 5px; text-align: left; font-family: Arial, sans-serif; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);';
 
     // Create h3
     let h3 = document.createElement('h3');
-    h3.style.cssText = 'margin-top: 0; font-size: 18px; color: #333;';
-    h3.textContent = 'Color Coding';
+    h3.style.cssText = 'margin-top: 0; font-size: 12px; color: #333;';
+    h3.textContent = 'Legend for Paths:';
     div.appendChild(h3);
 
     // Create p elements
@@ -196,7 +349,7 @@ function createLegend() {
         p.style.cssText = 'font-size: 12px; margin: 10px 0; line-height: 1.5em; color: #333;';
 
         let span = document.createElement('span');
-        span.style.cssText = 'display: inline-block; width: 18px; height: 18px; margin-right: 10px; vertical-align: middle; background-color: ' + colorDict[key] + ';';
+        span.style.cssText = 'display: inline-block; width: 12px; height: 12px; margin-right: 10px; vertical-align: middle; background-color: ' + colorDict[key] + ';';
         span.textContent = '\u00A0\u00A0\u00A0'; // Non-breaking spaces
 
         p.appendChild(span);
